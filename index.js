1ad78569b6a7c7b7d0001bd9170e30d4a7f913c9
Caroline Lanford <caroline.lanford@gmail.com>
2018-02-08 00:25:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var customerName = `bob`;
 
 function upperCaseCustomerName(customerName){
-  return customerName.toUpperCase;
+  console.log(customerName.toUpperCase;)
 }
 
 function setBestCustomer(){
